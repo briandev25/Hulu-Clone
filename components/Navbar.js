3c0b58ba59fa8b7e50d3d382b80d0c375c53d124
briandev25 <briankips34@gmail.com>
2022-02-25 16:21:15 +0300

    Completed the hulu clone

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import request from '../utils/request'
+import requests from '../utils/requests'
 import { useRouter } from 'next/router'
 
 function Navbar() {
@@ -7,7 +7,7 @@ function Navbar() {
   return (
         <nav className='relative'>
         <div className='flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide'>
-            {Object.entries(request).map(([key,{ title,url }]) =>(
+            {Object.entries(requests).map(([key,{ title,url }]) =>(
                 <h2 onClick={() => router.push(`/?genre=${key}`)} className='last:pr-4 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500'>{title}</h2>
             ))}
         </div>
